e33685151eb05a7efd8ee6925ab509d1dc7be055
ryocs <58067789+ryocs@users.noreply.github.com>
2020-10-29 11:36:00 +0100

    Update start.bat

--- client/start.bat
+++ client/start.bat
@@ -5,5 +5,5 @@ set CHROME_EXE=c:\Program Files (x86)\Google\Chrome\Application\chrome.exe
 
 set CHROME_SETTINGS=
 REM Path to the index.html file
-set CHROME_SETTINGS=%CHROME_SETTINGS% --app=C:\PERS\ChaddyGit\chaddy\client\index.html
+set CHROME_SETTINGS=%CHROME_SETTINGS% --app=<path-to-indexhtml>
 start "" "%CHROME_EXE%" %CHROME_SETTINGS%
